fff60a1ef90cb33bbaa648d54ad3ef23bb7a6c7f
begginerN1 <vkhintibidze@gmail.com>
2023-12-09 21:14:47 +0400

    slider has been finished

--- frontend/src/components/slider/Slider.js
+++ frontend/src/components/slider/Slider.js
@@ -1,19 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import{useNavigate}from 'react-router-dom'
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
 import { sliderData } from './slider-data';
+import "./Slider.scss"
 
 const Slider = () => {
 
     const [currentSlide, setCurrentSlide] = useState(0);
+    const navigate = useNavigate();
+
+    const slideLength = sliderData.length;
+    const autoScroll = true;
+    let slideInterval;
+    const intervalTime = 5000;
 
     const prevSlide = () => {
-        
+        setCurrentSlide(currentSlide===0 ? slideLength -1:  currentSlide - 1);
     }
     const nextSlide = () => {
-        
+        setCurrentSlide(currentSlide===slideLength-1? 0: currentSlide + 1);
     }
 
-  return (
+    useEffect(() => {
+      setCurrentSlide(0)  
+    },[])
+    
+    useEffect(() => {
+        if (autoScroll) {
+            const auto = () => {
+              slideInterval=setInterval(nextSlide,intervalTime)
+            }
+            auto();
+        }; 
+        return ()=>clearInterval(slideInterval)
+    },[currentSlide,intervalTime,autoScroll])
+
+
+    return (
+        <> 
+            {/* <div>
+                <img src="./w.jpg" alt="my file should be here" />
+            </div> */}
+            
       <div className='slider'>
           <AiOutlineArrowLeft className='arrow prev' onClick={prevSlide}/>
           <AiOutlineArrowRight className='arrow next' onClick={nextSlide} />
@@ -22,11 +50,11 @@ const Slider = () => {
               sliderData.map((slide, index) => {
                   const { image, heading, desc } = slide;
                   return (
-                      <div key={index} className={index===currentSlide?"slide current":"slide"}>
+                      <div key={index} className={index === currentSlide ? "slide current" : "slide"}>
+                          <img src={image} alt="slide" />
                           {index === currentSlide && (
                               <>
                                   <img src={image} alt="slide" />
-                                  <p>{`"${image}"`}</p>
                                   <div className='content'>
                                       <span className="span1"></span>
                                       <span className="span2"></span>
@@ -35,7 +63,7 @@ const Slider = () => {
                                       <h2>{heading}</h2>
                                       <p>{desc}</p>
                                       <hr />
-                                      <button className='--btn --btn-primary'>
+                                      <button className='--btn --btn-primary' onClick={()=>navigate('/shop')}>
                                           Shop Now
                                       </button>
                                   </div>
@@ -45,7 +73,7 @@ const Slider = () => {
                   )
              }) 
           }
-    </div>
+    </div></>
   )
 }
 
--- frontend/src/components/slider/slider-data.js
+++ frontend/src/components/slider/slider-data.js
@@ -1,32 +1,59 @@
 // https://i.ibb.co/g4RDXbZ/phone2.jpg
 
+// export const sliderData = [
+//   {
+//     image: "../../assets/white-shoe.png",
+//     heading: "Shoes Villa",
+//     desc: "Up to 30% off on all onsale proucts.",
+//   },
+//   {
+//     image: "../../assets/gadget.jpg",
+//     heading: "Hot Gadgets",
+//     desc: "Up to 30% off on all onsale proucts.",
+//   },
+//   {
+//     image: "../../assets/men-wear.jpg",
+//     heading: "Men Fashion",
+//     desc: "Up to 30% off on all onsale proucts.",
+//   },
+//   {
+//     image: "../../assets/phone.jpg",
+//     heading: "Android Phones",
+//     desc: "Up to 30% off on all onsale proucts.",
+//   },
+//   {
+//     image: "../../assets/blue-shoe.jpg",
+//     heading: "Nike Sneakers",
+//     desc: "Up to 30% off on all onsale proucts.",
+//   },
+// ];
+
+
+
 export const sliderData = [
   {
-    image: "../../white-shoe.png",
+    image:  "https://i.ibb.co/D1TvpVL/white-shoe.jpg",
     heading: "Shoes Villa",
     desc: "Up to 30% off on all onsale proucts.",
   },
   {
-    image: "../../assets/gadget.jpg",
+    image: "https://i.ibb.co/Bz5v6xs/gadget.jpg",
     heading: "Hot Gadgets",
     desc: "Up to 30% off on all onsale proucts.",
   },
   {
-    image: "../../assets/men-wear.jpg",
+    image: "https://i.ibb.co/RThT5KJ/men-wear.jpg",
     heading: "Men Fashion",
     desc: "Up to 30% off on all onsale proucts.",
   },
   {
-    image: "../../assets/phone.jpg",
+    image: "https://i.ibb.co/ZVQHTjk/phone.jpg",
     heading: "Android Phones",
     desc: "Up to 30% off on all onsale proucts.",
   },
   {
-    image: "../../assets/blue-shoe.jpg",
+    image: "https://i.ibb.co/tYcmTKr/blue-shoe.jpg",
     heading: "Nike Sneakers",
     desc: "Up to 30% off on all onsale proucts.",
   },
 ];
-
-
-// frontend\src\assets\blue-shoe.jpg https://github.com/begginerN1/Adv-Ecommerce-MERN/blob/main/frontend/src/assets/white-shoe.png
